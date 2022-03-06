f732bad6f5c9ad306995bb59ec3ebf593809ed14
TP <TP@IC-IIG-DPC-16.intranet.epfl.ch>
2022-03-06 12:50:37 +0100

    begin forward button and add new materials

--- VR Game/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ VR Game/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Forward
+        {
+            get
+            {
+                return SteamVR_Actions.default_Forward;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
