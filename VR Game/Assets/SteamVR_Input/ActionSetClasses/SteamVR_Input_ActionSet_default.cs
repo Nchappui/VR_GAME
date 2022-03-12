2609699564d03ad864cbc2cfe16970fbf9b49260
TP <TP@IC-IIG-DPC-16.intranet.epfl.ch>
2022-03-12 12:53:17 +0100

    move using touchpad

--- VR Game/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ VR Game/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 touchpadtouch
+        {
+            get
+            {
+                return SteamVR_Actions.default_touchpadtouch;
+            }
+        }
+        
         public virtual SteamVR_Action_Boolean Forward
         {
             get
