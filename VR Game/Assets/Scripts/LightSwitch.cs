bf8d1429c987bda3e190999e24b3651ae82463c9
TP <TP@IC-IIG-DPC-16.intranet.epfl.ch>
2022-04-30 21:47:11 +0200

    Add a light switch

---
+++ VR Game/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    private Light light;
+    private float init_range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = this.GetComponentInChildren<Light>();
+        init_range = light.range;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Switch_Light()
+    {
+        if (light.range > 0) light.range = 0;
+        else light.range = init_range;
+    }
+}
