e919b940374f950ae2adbd552d7b4b9f2d1c2afc
TP <TP@IC-IIG-DPC-16.intranet.epfl.ch>
2022-05-15 21:50:34 +0200

    Scripts for 3rd puzzle of outside world

---
+++ VR Game/Assets/Scripts/CalculateWeight.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CalculateWeight : MonoBehaviour
+{
+    private float weight=0;
+    public TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.TryGetComponent(out Weight w))
+        {
+            weight += w.weight;
+            text.text = weight.ToString();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out Weight w))
+        {
+            weight -= w.weight;
+            text.text = weight.ToString();
+        }
+    }
+}
---
+++ VR Game/Assets/Scripts/Weight.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+    public float weight = 1f;
+}
